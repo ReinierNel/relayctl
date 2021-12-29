abe1a740d3a07a016a34f30210523d636d023c91
ReinierNel <38002999+ReinierNel@users.noreply.github.com>
2021-12-29 11:05:25 +0200

    updated install dir to /etc/relayctl
    /home/pi might not be present in some instances, also added Parimiterisation of relays and scheduler_frequency

--- settings.sh
+++ settings.sh
@@ -1,12 +1,12 @@
 #!/bin.bash
 # set the GPIO PIN that each relay is connected to
 # Note that pisition 1 = relay 0 and so on
-relays=(22 23)
+relays=(__GPIO_PIN__)
 # where where is all the scripts located
-wokring_dir="/home/pi"
+wokring_dir="/etc/relayctl"
 # where is your scedule file located
-schedule_file_path="/home/pi/schedule.list"
+schedule_file_path="/etc/relayctl/schedule.list"
 # where sould the logs be stored defaults to working directory
-log_file_path="/home/pi/relayctl.log"
+log_file_path="/etc/relayctl/relayctl.log"
 # scheduler frequency
-scheduler_frequency="10"
+scheduler_frequency="__SCHEDULAR_FREQUEMCY__"
